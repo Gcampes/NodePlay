abed0d7673eebf76ac84d37e6dd0f7d1b32c70d4
Gcampes <gabrieldecampes@gmail.com>
2015-08-27 02:46:44 -0300

    Exercises to 12

--- http-file-server.js
+++ http-file-server.js
@@ -1,5 +1,6 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer(function callback(request, response){
-
+  fs.createReadStream(process.argv[3]).pipe(response);
 })
-server.listen(8000);
+server.listen(process.argv[2]);
